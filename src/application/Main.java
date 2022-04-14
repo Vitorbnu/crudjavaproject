a4bcb90a57baa78772bd363451abe0e9d17bccb3
Vitor da Silva <Vitor_slv@outlook.com>
2022-04-14 00:28:31 -0300

    UserView added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,28 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/UserView.fxml"));
+			Parent parent = loader.load();
+			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Application");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
